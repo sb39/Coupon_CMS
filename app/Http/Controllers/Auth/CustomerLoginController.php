b3e28b8574acb7838c95083dd2e3b5cc34dbc39d
sb39 <biswassrijeet99@gmail.com>
2019-04-16 11:53:31 +0530

    customer login made

--- app/Http/Controllers/Auth/CustomerLoginController.php
+++ app/Http/Controllers/Auth/CustomerLoginController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Auth;
-use Validator;
-
+// use Validator;
 class CustomerLoginController extends Controller
 {
     public function __construct()
@@ -20,23 +18,15 @@ class CustomerLoginController extends Controller
     }
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $this->validate($request, [
             'email' => 'required|email',
             'password' => 'required|min:6'
         ]);
-        // $this->Validator($request, [
-        //     'email' => 'required|email',
-        //     'password' => 'required|min:6'
-        // ]);
 
-        // if(Auth::guard('customer')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember))
-        // {
-        //     return redirect()->intended(route('customer.dashboard'));
-        // }
-        // return redirect()->back()->withInput($request->only('email', 'remember'));
-        if ($validator->fails()) {
-            return 123;
+        if(Auth::guard('customer')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember))
+        {
+            return redirect()->intended(route('customer.dashboard'));
         }
-        return redirect()->intended(route('customer.dashboard'));
+        return redirect()->back()->withInput($request->only('email', 'remember'));
     }
 }
